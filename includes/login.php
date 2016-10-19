6df123c8ffcd480cd96349bc92d07a3c4bf1a81d
Kukix <kepetrova@hotmail.com>
2016-10-19 15:54:18 +0200

    backend register validation
    updated htaccess on server side to deny access to indexing

--- includes/login.php
+++ includes/login.php
@@ -51,6 +51,9 @@ if (isset($_POST["function"])) {
 		$password2 = $_POST['password2'];
 		$email = $_POST['email'];
 		
+		$userError;
+		$passError;
+		$emailError;
 		//Secure password using salt. 
 		$hash = hash('sha256', $password1);
 		 
@@ -59,32 +62,103 @@ if (isset($_POST["function"])) {
 		$text = md5(uniqid(rand(), true));
 		return substr($text, 0, 20);
 		}
-		 
-		$salt = createSalt();
-		$password = hash('sha256', $salt . $hash);
-		 
-		//sanitize username
-		$username = mysqli_real_escape_string($con,$username);
-		$result = mysqli_query($con, "SELECT count(*) FROM users WHERE username='$username' GROUP BY username");
-		$rowcount = mysqli_num_rows($result);
-		//$querya = mysqli_query($con,"SELECT count(*) FROM member WHERE username='$username'");
-		if ($rowcount != 0)
+		
+		function validateName($name)
 		{
-		  //header('Location:registration.html');
-		  $message = "Username already exists.";
-		}
-		else{
-			if($password1 != $password2){
-				$message =  "Your passwords do not match.";
-				//header('Location: registration.html');
+			global $userError;
+			global $con;
+			if(empty($name))
+			{
+				$userError = "Name required.";
+				return false;
 			}
-			else{
-					$query = "INSERT INTO users ( username, password, email, salt )
+			else if(strlen($name)>25)
+			{
+				$userError = "Name cannot be more than 25 characters long";
+				return false;
+			}
+			else
+			{
+				$result = mysqli_query($con, "SELECT count(*) FROM users WHERE username='$name' GROUP BY username");
+				if(mysqli_num_rows($result) != 0)
+				{
+					$userError = "Account with this username already exists.";
+					return false;
+				}
+			}
+			return true;
+		}
+		
+		function validatePassword($pass1, $pass2)
+		{
+			global $passError;
+			$reg = "/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/";
+			if(empty($pass1))
+			{
+				$passError = "Password required.";
+				return false;
+			}else if(!preg_match($reg, $pass1))
+			{
+				$passError = "Password must be at least 8 characters long and must contain at least 1 upper case, 1 lower case and 1 digit";
+				return false;
+			}else if(empty($pass2))
+			{
+				$passError = "Please retype password.";
+				return false;
+			}else if($pass1 != $pass2)
+			{
+				$passError = "Passwords do not match.";
+				return false;
+			}
+			return true;
+		}
+		
+		function validateEmail($email1)
+		{
+		global $emailError;
+		global $con;
+			$reg = "/[A-Za-z0-9_-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}/";
+			if(empty($email1))
+			{
+				$emailError = "Email required.";
+				return false;
+			}
+			else if(!preg_match($reg, $email1))
+			{
+				$emailError = "Invalid email.";
+				return false;
+			}
+			else
+			{
+				$result = mysqli_query($con, "SELECT count(*) FROM users WHERE email ='$email1' GROUP BY email");
+				if(mysqli_num_rows($result) != 0)
+				{
+					$emailError = "Account with this email already exists.";
+					return false;
+				}
+			}
+			return true;
+		}
+		 
+		 if(!validateName($username))
+		 {
+			$message = $userError;
+		 }else if(!validatePassword($password1, $password2))
+		 {
+			$message = $passError;
+		 }else if(!validateEmail($email))
+		 {
+			$message = $emailError;
+		 }
+		 else
+		 {
+			$salt = createSalt();
+			$password = hash('sha256', $salt . $hash);
+			$query = "INSERT INTO users ( username, password, email, salt )
 					VALUES ( '$username', '$password', '$email', '$salt' );";
 					mysqli_query($con, $query);
 					$message =  "Succesfully registered";
-			}
-		}
+		 }
     }
 }
 ?>
